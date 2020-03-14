c8ffca554cd2ec6be44288828a6af1dd25ed766e
Ilce Medina <ilce.medn@gmail.com>
2020-03-14 00:05:18 -0600

    feat(chapter-one): added more content to chapter one

--- storytime/first_chapter.py
+++ storytime/first_chapter.py
@@ -1,1 +1,3 @@
 print("Había una vez, una dulce y pequeña campesina que se encontro un ganso dorado.")
+
+print("La pequeña campesina coloco una pequeña y fresca rana verde que encontró aun lado de la vereda en su canasta.")
